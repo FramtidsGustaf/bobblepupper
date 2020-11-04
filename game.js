63b98c17e31360b5c06c71a9ca4e588b6625024f
GustafJ <gustaf89@hotmail.se>
2020-11-04 13:10:47 +0100

    deleted button

--- game.js
+++ game.js
@@ -1,13 +1,7 @@
 class Game {
   constructor() {
     this.ball = new Ball();
-    this.button = document.getElementById("button");
     this.autoCreatingBalls(this.ball);
-    window.addEventListener("click", (e) => {
-      if (e.target === button) {
-        this.ball.createBall();
-      }
-    });
   }
   autoCreatingBalls(ball) {
     setInterval(() => {
