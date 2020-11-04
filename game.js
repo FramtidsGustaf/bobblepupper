3a5a7ab25ed0cc82f694dc3e40ecd892202c65b8
GustafJ <gustaf89@hotmail.se>
2020-11-04 13:12:55 +0100

    changed the rate of creating balls

--- game.js
+++ game.js
@@ -6,6 +6,6 @@ class Game {
   autoCreatingBalls(ball) {
     setInterval(() => {
       ball.createBall();
-    }, Math.floor(Math.random() * 500) + 100);
+    }, Math.floor(Math.random() * 1000) + 100);
   }
 }
