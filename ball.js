86d3a08a4bc29ed115ff8337d019974041f82719
GustafJ <gustaf89@hotmail.se>
2020-11-05 20:21:30 +0100

    now deletes every evenlistener and is getting harder for every 1 minute

--- ball.js
+++ ball.js
@@ -4,11 +4,13 @@ class Ball {
     this.score = new Score();
     this.colorArray = [`#e3b505`, `#95190c`, `#610345`, `#107e7d`, `#044B7f`];
     this.playField = document.getElementById("playField");
+    this.playFieldWidth = document.getElementsByTagName("body")[0].clientWidth;
   }
   createBall() {
     let ball = document.createElement("div");
     let rnd = Math.floor(Math.random() * 100) + 50;
-    let rndSpeed = Math.floor(Math.random() * 20) + 5;
+    let rndSpeed =
+      Math.floor(Math.random() * (this.playFieldWidth / 100)) + this.playFieldWidth / 110;
     let rndTop = Math.floor(Math.random() * 70) + 10;
     let rndColor = this.colorArray[Math.floor(Math.random() * 5)];
     let isClicked = false;
@@ -25,15 +27,17 @@ class Ball {
       ball.style.left = `100%`;
       ball.style.transform = "translate(-100%)";
     }, 100);
-    document.addEventListener("click", (e) => {
-      if (e.target === ball) {
-        ball.remove();
-        this.explosion.explode(e.clientX, e.clientY, rndColor);
-        isClicked = true;
-        this.score.score++;
-        this.score.currentScoreOutput();
-      }
-    });
+
+    let removeEnemy = (event) => {
+      ball.removeEventListener("click", removeEnemy);
+      ball.remove();
+      this.explosion.explode(event.clientX, event.clientY, rndColor);
+      isClicked = true;
+      this.score.score++;
+      this.score.currentScoreOutput();
+    };
+
+    ball.addEventListener("click", removeEnemy);
     setTimeout(() => {
       if (!isClicked) {
         ball.remove();
@@ -45,7 +49,8 @@ class Ball {
   createSquare() {
     let square = document.createElement("div");
     let rnd = Math.floor(Math.random() * 100) + 50;
-    let rndSpeed = Math.floor(Math.random() * 10) + 3;
+    let rndSpeed =
+      Math.floor(Math.random() * (this.playFieldWidth / 130)) + this.playFieldWidth / 140;
     let rndTop = Math.floor(Math.random() * 70) + 10;
     let rndColor = this.colorArray[Math.floor(Math.random() * 5)];
     let isClicked = false;
@@ -62,15 +67,17 @@ class Ball {
       square.style.left = `100%`;
       square.style.transform = "translate(-100%)";
     }, 100);
-    document.addEventListener("click", (e) => {
-      if (e.target === square) {
-        square.remove();
-        this.explosion.explode(e.clientX, e.clientY, rndColor);
-        isClicked = true;
-        this.score.score++;
-        this.score.currentScoreOutput();
-      }
-    });
+
+    let removeEnemy = (event) => {
+      square.removeEventListener("click", removeEnemy);
+      square.remove();
+      this.explosion.explode(event.clientX, event.clientY, rndColor);
+      isClicked = true;
+      this.score.score++;
+      this.score.currentScoreOutput();
+    };
+
+    square.addEventListener("click", removeEnemy);
     setTimeout(() => {
       if (!isClicked) {
         square.remove();
--- game.js
+++ game.js
@@ -4,25 +4,38 @@ class Game {
     this.startScreen(this.autoCreatingBalls, this.autoCreatingSquares, this.ball);
   }
   startScreen(autoCreatingBalls, autoCreatingSquares, ball) {
+    let body = document.getElementsByTagName("body")[0];
+    console.log(body.clientWidth);
     let start = document.getElementById("startScreen");
     let startButton = document.getElementById("startButton");
     let title = document.getElementById("title");
-    document.addEventListener("click", (e) => {
-      if (e.target === startButton) {
-        autoCreatingBalls(ball);
-        setTimeout(() => {
-          start.style.opacity = "0";
-          title.classList.remove("invisible");
-          title.classList.add("titleColor");
-        }, 100);
-        setTimeout(() => {
-          start.remove();
-        }, 500);
-        setTimeout(() => {
-          autoCreatingSquares(ball);
+    let counter = 1;
+    let difficultyIncreaser = () => {
+      autoCreatingBalls(ball);
+      setTimeout(() => {
+        start.style.opacity = "0";
+        title.classList.remove("invisible");
+        title.classList.add("titleColor");
+      }, 100);
+      setTimeout(() => {
+        start.remove();
+        startButton.removeEventListener("click", difficultyIncreaser);
+      }, 500);
+      setTimeout(() => {
+        autoCreatingSquares(ball);
+        setInterval(() => {
+          if (counter % 2 == 0) {
+            autoCreatingBalls(ball);
+            counter++;
+          } else {
+            autoCreatingSquares(ball);
+            counter++;
+          }
         }, 60000);
-      }
-    });
+      }, 60000);
+    };
+
+    startButton.addEventListener("click", difficultyIncreaser);
   }
   autoCreatingBalls(ball) {
     setInterval(() => {
